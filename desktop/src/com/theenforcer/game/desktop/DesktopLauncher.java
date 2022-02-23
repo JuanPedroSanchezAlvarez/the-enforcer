d32626ebd60480f733e3c35b0f58875069deee18
JuanPedroSanchezAlvarez <juanpedrosanchez1989@gmail.com>
2022-02-23 17:22:08 +0100

    Favicon

--- desktop/src/com/theenforcer/game/desktop/DesktopLauncher.java
+++ desktop/src/com/theenforcer/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.theenforcer.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.theenforcer.game.TheEnforcerGame;
@@ -9,6 +10,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = ConstantsConfiguration.GAME_TITLE;
+		config.addIcon("img/favicon.png", Files.FileType.Internal);
 		config.width = ConstantsConfiguration.SCREEN_WIDTH;
 		config.height = ConstantsConfiguration.SCREEN_HEIGHT;
 		new LwjglApplication(new TheEnforcerGame(), config);
